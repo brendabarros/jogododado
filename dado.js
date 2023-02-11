c118c7e278ebeef8ae2b56a6a8dc73f01d89d545
brendabarros <85901562+brendabarros@users.noreply.github.com>
2023-02-11 00:41:46 -0300

    Resultados dinâmicos
    adicionado um loop para adicionar dinamicamente a quantidade desejada de dados

--- dado.js
+++ dado.js
@@ -1,44 +1,56 @@
+// NUMERO DE DADOS
+var i = 0;
+for (; i < 6; i++) {
 
-function jogar() {
-    var pegardado = document.getElementById('face')
-    var sorteio = Math.floor(Math.random() * 6 + 1);
-    
-    
-    switch (sorteio) {
-        case 1:
-            pegardado.src = "imagens/face 1.png";
-
-            break;
-        case 2:
-            pegardado.src = "imagens/face 2.png";
-
-            break;
-        case 3:
-            pegardado.src = "imagens/face 3.png";
-
-            break;
-        case 4:
-            pegardado.src = "imagens/face 4.png";
-
-            break;
-        case 5:
-            pegardado.src = "imagens/face 5.png";
-
-            break;
-        case 6:
-            pegardado.src = "imagens/face 6.png";
-
-            break;
-
-
-
-        default:
-            // caso contrário
-            break
-
-    }
+    sorteio = Math.floor(Math.random() * 10 + 1);
     console.log(sorteio)
+    if (sorteio==sorteio){
+        resultado= jogar()
+    }
 
+    
 
 }
 
+// IMAGENS RELACIONADAS AOS NUMEROS SORTEADOS
+function jogar() {
+    
+     htmlid= ["imagem1","imagem2","imagem3","imagem4","imagem5","imagem6"];
+     
+    
+    switch (sorteio) {
+        
+    
+    case 1:
+    document.getElementById(htmlid[0]).innerHTML= "<img src='imagens/face 1.png'>.<h1>Falha Crítica &#x1F62D;<h1/>"
+
+    break;
+    case 2:
+    document.getElementById(htmlid[1]).innerHTML = "<img src='imagens/face 2.png'>"
+
+    break;
+    case 3:
+    document.getElementById(htmlid[2]).innerHTML = "<img src='imagens/face 3.png'>"
+
+    break;
+    case 4:
+    document.getElementById(htmlid[3]).innerHTML = "<img src='imagens/face 4.png'>"
+
+    break;
+    case 5:
+    document.getElementById(htmlid[4]).innerHTML = "<img src='imagens/face 5.png'>"
+
+    break;
+    case 6:
+    document.getElementById(htmlid[5]).innerHTML = "<img src='imagens/face 6.png'>.<h1>Sucesso &#128526;<h1/>"
+
+    break;
+
+
+
+    default:
+    // caso contrário
+    break
+
+}
+}
